17e277048a87973e82be02e1bdffa2dc9b821439
Argyris <arkatsy@gmail.com>
2024-03-29 05:35:54 +0200

    test: create testing env

---
+++ src/tests/country-page.test.tsx
@@ -0,0 +1,7 @@
+import { test, expect } from "vitest";
+import CountryPage from "@/country-page";
+import { render, userEvent } from "@/tests/utils";
+
+test("test", () => {
+  expect(1).toBe(1);
+});
