628bc92767e17fbbe9d886470c4843e14de68967
Argyris <arkatsy@gmail.com>
2024-03-29 04:02:25 +0200

    chore: remove logs from build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  esbuild: {
+    pure: mode === "production" ? ["console.log"] : [],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
